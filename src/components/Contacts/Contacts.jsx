ecf9dfcf4f757f5db2b9e2b25e5ed07ebd82f37c
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-07-15 13:54:49 +0300

    edit after mentors remark

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import ListItems from 'components/ListItems/ListItems';
 import { Box } from 'components/Form/Form.styled';
 import { ContactsList } from './Contacts.styled';
@@ -11,3 +12,14 @@ export function Contacts(props) {
     </Box>
   );
 }
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
